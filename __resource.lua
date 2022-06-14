f2e9020e9dbfb53fd24dc5c12a0a74c4891236a2
LudiYoutuber65 <67953605+LudiYoutuber65@users.noreply.github.com>
2022-06-14 19:32:17 +0200

    https://discord.gg/TTuwDdVB7t
    https://discord.gg/TTuwDdVB7t

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Ludi početne komande'
 
-version '1.0.0'
+version '1.0.1'
 
 server_script {
 '@es_extended/locale.lua',
